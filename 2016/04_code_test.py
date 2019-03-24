2dbc14da9af7e17f3793c8d8900b1bd117386b1b
Paweł Chudzik <pawel.chudzik@gmail.com>
2019-03-24 20:55:35 +0100

    2016 day 04

---
+++ 2016/04_code_test.py
@@ -0,0 +1,49 @@
+import importlib
+import pytest
+
+room_parser = importlib.import_module("04_code").room_parser
+count_sectors_of_real_rooms = importlib.import_module("04_code").count_sectors_of_real_rooms
+Room = importlib.import_module("04_code").Room
+
+
+@pytest.mark.parametrize(
+    "input, name, sector, checksum", [
+        ("aaaaa-bbb-z-y-x-123[abxyz]", "aaaaa-bbb-z-y-x", 123, "abxyz"),
+        ("a-b-c-d-e-f-g-h-987[abcde]", "a-b-c-d-e-f-g-h", 987, "abcde"),
+        ("not-a-real-room-404[oarel]", "not-a-real-room", 404, "oarel")])
+def test_parse_room(input, name, sector, checksum):
+    room = room_parser(input)
+
+    assert room.name == name
+    assert room.sector == sector
+    assert room.checksum == checksum
+
+
+@pytest.mark.parametrize(
+    "room_str, is_real", [
+        ("aaaaa-bbb-z-y-x-123[abxyz]", True),
+        ("a-b-c-d-e-f-g-h-987[abcde]", True),
+        ("not-a-real-room-404[oarel]", True),
+        ("totally-real-room-200[decoy]", False)
+    ])
+def test_is_real_room(room_str, is_real):
+    room = room_parser(room_str)
+
+    assert room.is_real == is_real
+
+
+def test_sum_real_room_sectors():
+    rooms = [
+        "aaaaa-bbb-z-y-x-123[abxyz]",
+        "a-b-c-d-e-f-g-h-987[abcde]",
+        "not-a-real-room-404[oarel]",
+        "totally-real-room-200[decoy]"
+    ]
+
+    assert count_sectors_of_real_rooms(rooms) == 1514
+
+
+def test_decrypt_name():
+    encrypted = "qzmt-zixmtkozy-ivhz-343[zimth]"
+
+    assert room_parser(encrypted).decrypted_name == "very encrypted name"
