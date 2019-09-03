4df47937f1061cf98349f10cac8663839fee6429
Paweł Chudzik <pawel.chudzik@gmail.com>
2019-09-03 14:08:29 +0200

    2017 day 4 part 2

--- 2017/4_high_entropy_passphrases_test.py
+++ 2017/4_high_entropy_passphrases_test.py
@@ -2,7 +2,8 @@ import importlib
 import pytest
 
 module = importlib.import_module("4_high_entropy_passphrases")
-is_valid = module.is_valid
+has_no_duplicated_words = module.has_no_duplicated_words
+has_no_anagrams = module.has_no_anagrams
 find_valid_passphases = module.find_valid_passphases
 
 
@@ -11,14 +12,43 @@ find_valid_passphases = module.find_valid_passphases
         ("aa bb cc dd ee", True),
         ("aa bb cc dd aa", False),
         ("aa bb cc dd aaa", True)])
-def test_is_valid(password, expected):
-    assert is_valid(password) == expected
+def test_has_no_duplicated_words(password, expected):
+    assert has_no_duplicated_words(password) == expected
 
 
-def test_find_valid_passphases():
-    assert find_valid_passphases((
-        "aa bb cc dd ee",
-        "aa bb cc dd aa",
-        "aa bb cc dd aaa")) == [
+@pytest.mark.parametrize(
+    "password, expected", [
+        ("abcde fghij", True),
+        ("abcde xyz ecdab", False),
+        ("a ab abc abd abf abj", True),
+        ("iiii oiii ooii oooi oooo", True),
+        ("oiii ioii iioi iiio", False),
+        ("refpcz uqt uqt uqt", False)
+    ])
+def test_has_no_anagrams(password, expected):
+    assert has_no_anagrams(password) == expected
+
+
+def test_find_valid_passphases_no_duplicated_words():
+    assert find_valid_passphases(
+        has_no_duplicated_words,
+        (
+            "aa bb cc dd ee",
+            "aa bb cc dd aa",
+            "aa bb cc dd aaa")) == [
                "aa bb cc dd ee",
                "aa bb cc dd aaa"]
+
+
+def test_find_valid_passphases_no_anagrams():
+    assert find_valid_passphases(
+        has_no_anagrams,
+        (
+            "abcde fghij",
+            "abcde xyz ecdab",
+            "a ab abc abd abf abj",
+            "iiii oiii ooii oooi oooo",
+            "oiii ioii iioi iiio")) == [
+               "abcde fghij",
+               "a ab abc abd abf abj",
+               "iiii oiii ooii oooi oooo"]
