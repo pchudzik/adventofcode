245c775d72ec584264c0e2684b963b587b038f80
Paweł Chudzik <pawel.chudzik@gmail.com>
2020-04-04 10:22:09 +0200

    2019 day 9 part 2

--- 2018/09_marble_mania.py
+++ 2018/09_marble_mania.py
@@ -1,51 +1,114 @@
+from collections import deque
+
+"""
+--- Day 9: Marble Mania ---
+
+You talk to the Elves while you wait for your navigation system to initialize. To pass the time, they introduce you to
+their favorite marble game.
+
+The Elves play this game by taking turns arranging the marbles in a circle according to very particular rules. The
+marbles are numbered starting with 0 and increasing by 1 until every marble has a number.
+
+First, the marble numbered 0 is placed in the circle. At this point, while it contains only a single marble, it is still
+a circle: the marble is both clockwise from itself and counter-clockwise from itself. This marble is designated the
+current marble.
+
+Then, each Elf takes a turn placing the lowest-numbered remaining marble into the circle between the marbles that are 1
+and 2 marbles clockwise of the current marble. (When the circle is large enough, this means that there is one marble
+between the marble that was just placed and the current marble.) The marble that was just placed then becomes the
+current marble.
+
+However, if the marble that is about to be placed has a number which is a multiple of 23, something entirely different
+happens. First, the current player keeps the marble they would have placed, adding it to their score. In addition, the
+marble 7 marbles counter-clockwise from the current marble is removed from the circle and also added to the current
+player's score. The marble located immediately clockwise of the marble that was removed becomes the new current marble.
+
+For example, suppose there are 9 players. After the marble with value 0 is placed in the middle, each player (shown in
+square brackets) takes a turn. The result of each of those turns would produce circles of marbles like this, where
+clockwise is to the right and the resulting current marble is in parentheses:
+
+[-] (0)
+[1]  0 (1)
+[2]  0 (2) 1 
+[3]  0  2  1 (3)
+[4]  0 (4) 2  1  3 
+[5]  0  4  2 (5) 1  3 
+[6]  0  4  2  5  1 (6) 3 
+[7]  0  4  2  5  1  6  3 (7)
+[8]  0 (8) 4  2  5  1  6  3  7 
+[9]  0  8  4 (9) 2  5  1  6  3  7 
+[1]  0  8  4  9  2(10) 5  1  6  3  7 
+[2]  0  8  4  9  2 10  5(11) 1  6  3  7 
+[3]  0  8  4  9  2 10  5 11  1(12) 6  3  7 
+[4]  0  8  4  9  2 10  5 11  1 12  6(13) 3  7 
+[5]  0  8  4  9  2 10  5 11  1 12  6 13  3(14) 7 
+[6]  0  8  4  9  2 10  5 11  1 12  6 13  3 14  7(15)
+[7]  0(16) 8  4  9  2 10  5 11  1 12  6 13  3 14  7 15 
+[8]  0 16  8(17) 4  9  2 10  5 11  1 12  6 13  3 14  7 15 
+[9]  0 16  8 17  4(18) 9  2 10  5 11  1 12  6 13  3 14  7 15 
+[1]  0 16  8 17  4 18  9(19) 2 10  5 11  1 12  6 13  3 14  7 15 
+[2]  0 16  8 17  4 18  9 19  2(20)10  5 11  1 12  6 13  3 14  7 15 
+[3]  0 16  8 17  4 18  9 19  2 20 10(21) 5 11  1 12  6 13  3 14  7 15 
+[4]  0 16  8 17  4 18  9 19  2 20 10 21  5(22)11  1 12  6 13  3 14  7 15 
+[5]  0 16  8 17  4 18(19) 2 20 10 21  5 22 11  1 12  6 13  3 14  7 15 
+[6]  0 16  8 17  4 18 19  2(24)20 10 21  5 22 11  1 12  6 13  3 14  7 15 
+[7]  0 16  8 17  4 18 19  2 24 20(25)10 21  5 22 11  1 12  6 13  3 14  7 15
+
+The goal is to be the player with the highest score after the last marble is used up. Assuming the example above ends
+after the marble numbered 25, the winning score is 23+9=32 (because player 5 kept marble 23 and removed marble 9, while
+no other player got any points in this very short example game).
+
+Here are a few more examples:
+
+10 players; last marble is worth 1618 points: high score is 8317
+13 players; last marble is worth 7999 points: high score is 146373
+17 players; last marble is worth 1104 points: high score is 2764
+21 players; last marble is worth 6111 points: high score is 54718
+30 players; last marble is worth 5807 points: high score is 37305
+
+What is the winning Elf's score?
+
+Your puzzle answer was 425688.
+
+--- Part Two ---
+
+Amused by the speed of your answer, the Elves are curious:
+
+What would the new winning Elf's score be if the number of the last marble were 100 times larger?
+
+Your puzzle answer was 3526561003.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+"""
+
+
 class Board:
     def __init__(self):
-        self.board = []
+        self.board = deque([0])
         self.current = 1
 
     def add_marble(self, marble):
         if marble % 23 == 0:
-            self.current = self._find_position_counter_clockwise(7 + 2)
-            extra_marble = self.board.pop(self.current)
-            self.current = self._find_position_clockwise(2)
-            return marble + extra_marble
-
-        if len(self.board) == 0:
-            self.board = [0, marble]
-        elif len(self.board) == 2:
-            self.board = [0, marble, *self.board[1:]]
-        elif len(self.board) == 3:
-            self.board = [0, *self.board[1:], marble]
+            self.board.rotate(7)
+            result = marble + self.board.pop()
+            self.board.rotate(-1)
+            return result
         else:
-            self.board.insert(self.current, marble)
-            self.current = self._find_position_clockwise(2)
+            self.board.rotate(-1)
+            self.board.append(marble)
 
-        return 0
-
-    def _find_position_counter_clockwise(self, count):
-        current = self.current
-        for _ in range(count):
-            current -= 1
-            if current <= 0:
-                current = len(self.board)
-        return current
-
-    def _find_position_clockwise(self, count):
-        if self.current + count > len(self.board):
-            return 1
-        return self.current + count
+            return 0
 
     def __repr__(self):
         return ",".join(map(str, self.board)) + f" ({self.current})"
 
 
-def play(board, players, marbles):
+def play(players, marbles):
+    board = Board()
     current_player = 0
     current_marble = 1
     scores = [0] * players
-    from time import time
 
-    start = time()
     while current_marble <= marbles:
         score = board.add_marble(current_marble)
         scores[current_player] += score
@@ -56,17 +119,12 @@ def play(board, players, marbles):
         if current_player >= players:
             current_player = 0
 
-        if int(current_marble) % 100_000 == 0:
-            now = time()
-            print(f"{current_marble} out of {marbles} ({len(board.board)}) in {now - start}")
-            start = time()
-
     return max(scores)
 
 
 if __name__ == "__main__":
-    max_score_1 = play(Board(), 411, 71170)
+    max_score_1 = play(411, 71170)
     print(f"part 1: {max_score_1}")
 
-    max_score_2 = play(Board(), 411, 71170 * 100)
+    max_score_2 = play(411, 71170 * 100)
     print(f"part 2: {max_score_2}")
